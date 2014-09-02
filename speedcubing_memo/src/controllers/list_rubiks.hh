9ccb9ee31bbb333fdfb46b75fddc08250c6cb29c
Gabriel Rea <gareve25@gmail.com>
2014-09-02 13:17:56 -0400

    Uploading new rubik states

--- speedcubing_memo/src/controllers/list_rubiks.hh
+++ speedcubing_memo/src/controllers/list_rubiks.hh
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 
 $moves = Vector{
-	Map {'name' => 'cuack_move', 'answer' => 'LRDE', 'id' => 3},
-	Map {'name' => 'cuack_move', 'answer' => 'LRDE', 'id' => 4}
+	Map {'name' => 'cuack_move123', 'answer' => 'LRDE', 'id' => 3},
+	Map {'name' => 'cuack_move123', 'answer' => 'LRDE', 'id' => 4}
 };
 echo json_encode($moves);
